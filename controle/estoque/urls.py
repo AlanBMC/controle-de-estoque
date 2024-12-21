dfb68b3369e91a4cea1cf113d2db1c1cc126a5b8
Alan Morais <alanbmc00@gmail.com>
2024-12-21 14:42:56 -0400

    feat: adicionando funçoes de cadastro login logout

--- controle/estoque/urls.py
+++ controle/estoque/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-
+from . import views
 urlpatterns = [
-    path
+    path('logout/', views.logout_view, name='logout'),
+    path('cadastro_user/', views.cadastro_usuario, name='cadastro'),
+    path('login_user/', views.login_user, name='login_user'),
 ]
--- controle/estoque/views.py
+++ controle/estoque/views.py
@@ -3,16 +3,39 @@ from django.contrib.auth import logout
 from django.contrib.auth import login as login_django
 from .models import Usuario
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 
 
 
 # Create your views here.
 def cadastro_usuario(request):
-    pass
+    if request.method == 'POST':
+        nome = request.POST.get('nome')
+        senha = request.POST.get('senha')
+        if User.objects.filter(username=nome):
+            return render(request, 'cadastro_login.html', {'mensagem_alerta': 'Nome de usario ja existe'})
+        else:
+            user = User.objects.create_user(username=nome, password=senha)
+            user.save()
+            usuario = Usuario(nome=nome, tipo_user='admin')
+            usuario.save()
+            return render(request, 'cadastro_login.html', {'mensagem_alerta': 'Usuario criado com sucesso'}) #redirecionar para login
+    else:
+        return render(request, 'cadastro_login.html')
 
-def login(request):
-    pass
+def login_user(request):
+    if request.method == 'POST':
+        nome = request.POST.get('nome')
+        senha = request.POST.get('senha')
+        user = authenticate(username=nome, password=senha)
+        if user:
+            login_django(request, user)
+            return render(request, 'home.html')
+    else:
+        return render(request, 'login.html')
 
-def logout(request):
-    pass
+def logout_view(request):
+    logout(request)
+    return redirect('login_user')
 
